c7eca5d9cc0b23dad9aa803939fbef03f4712641
luyongkang <2661089533@qq.com>
2021-09-14 18:56:02 +0800

    79

--- 79.cpp
+++ 79.cpp
@@ -1,2 +1,27 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <stack>
 using namespace std;
+
+void checkAround(int x, int y, vector<vector<char>> &board, stack<int> &temp, char c)
+{
+	int m = board.size() - 1;
+	int n = board.at(0).size() - 1;
+}
+
+bool exist(vector<vector<char>> &board, string word)
+{
+	int m = board.size() - 1;
+	int n = board.at(0).size() - 1;
+	stack<int> temp;
+	for (; m != -1; m--)
+	{
+		for (; n != -1; n--)
+		{
+			if (board[m][n] == word.at(0))
+			{
+				checkAround(m, n, board, temp, );
+			}
+		}
+	}
+}
